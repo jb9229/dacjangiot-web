2f23b4cf20521fa53e5ad4de201bcab7c6f56c54
jb9229 <jb9229@gmail.com>
2018-01-07 16:09:18 +0900

    Change Repository Name  1. Server File commit

--- dacjangiot-app/src/main/java/jb/smartfarm/dacjang/DacjangRepository.java
+++
@@ -1,9 +0,0 @@
-package jb.smartfarm.dacjang;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Created by test on 2017-10-14.
- */
-public interface DacjangRepository extends JpaRepository<DacjangInfo, Long> {
-}
